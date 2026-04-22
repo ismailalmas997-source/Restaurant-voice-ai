20f0a883ccf422896c164f5bbeef6b3f6a074013
ismailalmas997-source <ismailalmas997@gmail.com>
2026-04-22 14:31:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,23 +9,33 @@ app.get("/", (req, res) => {
 });
 
 app.post("/voice", async (req, res) => {
-  console.log("Call received");
+  console.log("Twilio callback received");
 
   const recordingUrl = req.body.RecordingUrl;
 
   if (recordingUrl) {
+    console.log("Recording URL:", recordingUrl);
+
     await fetch("https://hook.eu1.make.com/niek035xjpedozut1nzt1xo8yi3wbfx5", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        recordingUrl: recordingUrl
+        recordingUrl: recordingUrl + ".mp3"
       })
     });
   }
 
-  res.send("OK");
+  res.set("Content-Type", "text/xml");
+  res.send(`
+    <Response>
+      <Say voice="alice" language="fr-FR">
+        Merci, votre commande a bien été enregistrée.
+      </Say>
+      <Hangup/>
+    </Response>
+  `);
 });
 
 const PORT = process.env.PORT || 8080;
